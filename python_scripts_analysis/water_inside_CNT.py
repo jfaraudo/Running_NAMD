708b5ceb92c8145c2a9d0bc5c246175ee5c3296e
Jordi Faraudo <jfaraudo@icmab.es>
2025-05-12 11:48:46 +0200

    Add files via upload

--- python_scripts_analysis/water_inside_CNT.py
+++ python_scripts_analysis/water_inside_CNT.py
@@ -3,16 +3,23 @@ import MDAnalysis as mda
 import matplotlib.pyplot as plt
 
 #Load trajectory
-simulacio = mda.Universe('imdnanotube.psf','autoimd.dcd')
+simulacio = mda.Universe('../input/system.psf','MDequil.dcd')
+
+#Output Data
+fitxer= open('Waterinside.dat', 'w')
+fitxer.write("# time(ps) Num_water\n")
 
 #init list
 time =[]
 waterin=[]
+i=0
 
 #Loop over frames calculating water inside as a funciton of time
 for ts in simulacio.trajectory:
     time.append(simulacio.trajectory.time)
     waterin.append(len(simulacio.select_atoms('type OT and prop abs z <6.7')))
+    fitxer.write(f'{round(time[i])} {waterin[i]}\n') #save
+    i=i+1
 
 #Plot
 plt.plot(time,waterin)
